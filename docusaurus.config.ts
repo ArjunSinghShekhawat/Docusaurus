f9525d2d6d05b6f9d8de845bfa19a7d27ade2803
Arjun Singh Shekhawat <121460096+ArjunSinghShekhawat@users.noreply.github.com>
2025-08-19 14:16:20 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,6 +50,7 @@ const config: Config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 'ALL',
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
